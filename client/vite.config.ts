20b498ad039519fabd74a9af8c38fa68a53c018f
fla <konsta.larionov.vanced@gmail.com>
2024-06-15 05:04:57 +0300

    docker buid

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,7 +1,9 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
+export default {
+  server: {
+    host: "0.0.0.0",
+    port: 3000,
+  },
+};
--- server/app.py
+++ server/app.py
@@ -272,4 +272,4 @@ def test():
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=5000)
