a4d2eb973a884cef892ff0eaaff1016bd7581a9e
fla <konsta.larionov.vanced@gmail.com>
2024-07-01 12:21:36 +0300

    db automate data deleting

--- server/routes/__init__.py
+++ server/routes/__init__.py
@@ -3,7 +3,22 @@ from .flight import flight_bp
 from .location import location_bp
 from .radius import radius_bp
 from .config import Config
+from apscheduler.schedulers.asyncio import AsyncIOScheduler
+from apscheduler.triggers.interval import IntervalTrigger
 import asyncpg
+import asyncio
+
+async def cleanup_old_records():
+    async with app.db_pool.acquire() as con:
+        await con.execute('''
+            DELETE FROM search_history
+            WHERE date_time < EXTRACT(EPOCH FROM (NOW() - INTERVAL '10 days'))
+        ''')
+
+def start_scheduler():
+    scheduler = AsyncIOScheduler()
+    scheduler.add_job(cleanup_old_records, IntervalTrigger(days=1))
+    scheduler.start()
 
 def create_app():
     app = Quart(__name__)
@@ -15,9 +30,14 @@ def create_app():
     @app.before_serving
     async def create_db_pool():
         app.db_pool = await asyncpg.create_pool(Config.DATABASE_URL)
-    
+        start_scheduler()
+
     @app.after_serving
     async def close_db_pool():
         await app.db_pool.close()
 
     return app
+
+if __name__ == "__main__":
+    app = create_app()
+    asyncio.get_event_loop().run_until_complete(app.run_task())
