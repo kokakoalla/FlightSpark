3a79931ea3d0689c3be2848b33cb3f7455a56e9e
fla <konsta.larionov.vanced@gmail.com>
2024-07-01 14:42:20 +0300

    autoremoove

--- server/routes/__init__.py
+++ server/routes/__init__.py
@@ -1,3 +1,4 @@
+# import logging
 from quart import Quart
 from .flight import flight_bp
 from .location import location_bp
@@ -7,17 +8,38 @@ from apscheduler.schedulers.asyncio import AsyncIOScheduler
 from apscheduler.triggers.interval import IntervalTrigger
 import asyncpg
 import asyncio
+import datetime
 
-async def cleanup_old_records():
+# logging.basicConfig(level=logging.INFO)
+# logger = logging.getLogger(__name__)
+
+async def cleanup_old_records(app):
     async with app.db_pool.acquire() as con:
+        current_time = datetime.datetime.now().timestamp()
+        time_10_days_ago = (datetime.datetime.now() - datetime.timedelta(days=10)).timestamp()
+        
+        logger.info(f"Current Unix time: {current_time}")
+        logger.info(f"Unix time 10 days ago: {time_10_days_ago}")
+
+        old_records = await con.fetch('''
+            SELECT * FROM search_history
+            WHERE date_time < $1
+        ''', time_10_days_ago)
+        
+        if old_records:
+            logger.info(f"Old records to be deleted: {old_records}")
+        else:
+            logger.info("No old records found.")
+
         await con.execute('''
             DELETE FROM search_history
-            WHERE date_time < EXTRACT(EPOCH FROM (NOW() - INTERVAL '10 days'))
-        ''')
+            WHERE date_time < $1
+        ''', time_10_days_ago)
+        logger.info("Old records deleted successfully.")
 
-def start_scheduler():
+def start_scheduler(app):
     scheduler = AsyncIOScheduler()
-    scheduler.add_job(cleanup_old_records, IntervalTrigger(days=1))
+    scheduler.add_job(cleanup_old_records, IntervalTrigger(days=1), args=[app]) # days=1
     scheduler.start()
 
 def create_app():
@@ -30,7 +52,7 @@ def create_app():
     @app.before_serving
     async def create_db_pool():
         app.db_pool = await asyncpg.create_pool(Config.DATABASE_URL)
-        start_scheduler()
+        start_scheduler(app)
 
     @app.after_serving
     async def close_db_pool():
@@ -40,4 +62,4 @@ def create_app():
 
 if __name__ == "__main__":
     app = create_app()
-    asyncio.get_event_loop().run_until_complete(app.run_task())
+    # asyncio.get_event_loop().run_until_complete(app.run_task())
