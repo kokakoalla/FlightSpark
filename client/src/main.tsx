4eb2d9614ef9ea207c4d2d4811e3c5a24236df2b
Ubuntu <ubuntu@ip-172-26-11-154.eu-central-1.compute.internal>
2024-07-24 17:21:17 +0000

    qwe

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -7,11 +7,12 @@ import Geo from "./components/CardInsperation.tsx/Geo.tsx";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <div className="logo w-full text-center py-4">
-  <span className="text-2xl md:text-3xl lg:text-4xl font-bold">
-    FlightSpark
-  </span>
-</div>
+   
+    <div className="logo w-full text-center py-4"> <span 
+      className="text-5xl md:text-6xl lg:text-7xl xl:text-7xl font-bold">
+        FlightSpark </span>
+    </div>
+
     <FlightSearch />
     <Geo />
 
