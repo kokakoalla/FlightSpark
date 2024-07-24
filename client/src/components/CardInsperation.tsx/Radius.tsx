8674cc843796651c972bba160d347f51f559d828
fla <konsta.larionov.vanced@gmail.com>
2024-07-24 19:41:29 +0400

    f.online

--- client/src/components/CardInsperation.tsx/Radius.tsx
+++ client/src/components/CardInsperation.tsx/Radius.tsx
@@ -2,7 +2,8 @@ import React, { useState, useEffect } from "react";
 import axios from "axios";
 import Card from "./Card";
 
-const API_BASE_URL = "http://localhost:8000/api";
+// const API_BASE_URL = "http://localhost:8000/api";
+const API_BASE_URL = "https://flightspark.online/api";
 
 // Define the structure of the API response
 interface SearchHistoryEntry {
