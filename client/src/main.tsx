8f43ecc5e9927efe2e7c1ceed12c1296faa480ed
kokoalla <konsta.larionov.vanced@gmail.com>
2024-07-25 21:26:23 +0300

    mt-10

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -8,7 +8,8 @@ import logo from "./assets/Spark.png";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <div className=" w-full flex justify-center py-4">
+    <div className=" w-full flex justify-center py-4 mt-10
+    ">
       <div className="logo text-4xl ">
         <img className="absolute w-24 -mt-12 -ml-3 marker:" src={logo} />
         FlightSpark
